f5de1a8e407002447801819ec83888c574147db5
Claudio Toyoshi Onohara <claudioonohara@gmail.com>
2020-05-06 07:44:33 -0400

    criacao de botoes e lista dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,30 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
-
   Widget build(BuildContext context) {
+    final perguntas = ['Qual é seu nome ?', 'Qual é seu endereço ?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Respota 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Respota 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Respota 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
