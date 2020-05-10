fcdd19f3ebc7d31aefcabe66654fc594bc837dfc
Claudio Toyoshi Onohara <claudioonohara@gmail.com>
2020-05-10 17:21:17 -0400

    adicionado funcionalidade de reiniciar

--- lib/main.dart
+++ lib/main.dart
@@ -44,8 +44,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
         _pontuacaoTotal += pontuacao;
       });
     }
+  }
 
-    print(_pontuacaoTotal);
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
   }
 
   bool get temPerguntaSelecionada {
@@ -64,7 +69,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                 perguntaSelecionada: _perguntaSelecionada,
                 quantoResponder: _responder,
               )
-            : Resultado(_pontuacaoTotal),
+            : Resultado(_pontuacaoTotal,_reiniciarQuestionario),
       ),
     );
   }
